113ce2bb709033c44f58d2bdf1bcb727eb5f463d
sraboni303 <sraboni391@gmail.com>
2021-04-14 15:36:40 +0600

    add active class to menu

--- resources/views/backend/layouts/menu.blade.php
+++ resources/views/backend/layouts/menu.blade.php
@@ -6,25 +6,25 @@
                 <li>
                     <a href="#"><i class="fe fe-home"></i> <span> Dashboard</span></a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('create.post') ? 'active' : '' }}">
                     <a href="{{ route('create.post') }}"> <i class="fe fe-document"></i> <span> Create Post</a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('list.posts') ? 'active' : '' }}">
                     <a href="{{ route('list.posts') }}"> <i class="fe fe-document"></i> <span> Posts List </a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('trashes.posts') ? 'active' : '' }}">
                     <a href="{{ route('trashes.posts') }}"> <i class="fe fe-document"></i> <span> Trashes </a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('category.backend') ? 'active' : '' }}">
                     <a href="{{ route('category.backend') }}"><i class="fe fe-document"></i> <span> Categories </a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('tag.backend') ? 'active' : '' }}">
                     <a href="{{ route('tag.backend') }}"><i class="fe fe-document"></i> <span>  Tags </a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('role.backend') ? 'active' : '' }}">
                     <a href="{{ route('role.backend') }}"><i class="fe fe-document"></i> <span>  Roles </a>
                 </li>
-                <li>
+                <li class="{{  request()->routeIs('member.backend') ? 'active' : '' }}">
                     <a href="{{ route('member.backend') }}"><i class="fe fe-document"></i> <span>  Members </a>
                 </li>
 
